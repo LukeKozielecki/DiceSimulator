4a103daa588aa835299b184c3dfb883db52d9275
Luke <lucjan.kozielecki@gmail.com>
2024-07-25 14:17:55 +0200

    Implementation of serializable Navigation, it's dependencies & auxiliary Routes file

--- app/src/main/java/luke/koz/dicesimulator/MainActivity.kt
+++ app/src/main/java/luke/koz/dicesimulator/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import luke.koz.dicesimulator.dicehublocation.presentation.HubScreen
 import luke.koz.dicesimulator.ui.theme.DiceSimulatorTheme
 import luke.koz.dicesimulator.utils.navigation.AppNavigationHost
 
